1be78116bb08853aa8e47f25efd57bf4b2a2e70c
RikiiB <143796850+RikiiB@users.noreply.github.com>
2024-05-28 23:28:25 +0200

    MAE-24 small changes for requesting permissions

--- app/src/main/java/nrw/florian/cookbook/FirstFragment.java
+++ app/src/main/java/nrw/florian/cookbook/FirstFragment.java
@@ -1,11 +1,18 @@
 package nrw.florian.cookbook;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -14,6 +21,12 @@ import nrw.florian.cookbook.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        locationPermissionRequest.launch(new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION});
+    }
 
     @Override
     public View onCreateView(
@@ -28,10 +41,9 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         binding.buttonFirst.setOnClickListener(v ->
                 NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
+                        .navigate(R.id.action_FirstFragment_to_weatherFragment)
         );
     }
 
@@ -41,4 +53,18 @@ public class FirstFragment extends Fragment {
         binding = null;
     }
 
+    ActivityResultLauncher<String[]> locationPermissionRequest = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+        Boolean fineLocationGranted = result.getOrDefault(ACCESS_FINE_LOCATION, false);
+        Boolean coarseLocationGranted = result.getOrDefault(ACCESS_COARSE_LOCATION, false); // TODO muss hier auch noch Background-Permission hin?
+        if (fineLocationGranted != null && fineLocationGranted) {
+            // Precise location access granted.
+            Toast.makeText(requireActivity(), R.string.permission_granted, Toast.LENGTH_SHORT).show();
+        } else if (coarseLocationGranted != null && coarseLocationGranted) {
+            // Only approximate location access granted.
+            Toast.makeText(requireActivity(), R.string.permission_granted, Toast.LENGTH_SHORT).show();
+        } else {
+            // No location access granted.
+            Toast.makeText(requireActivity(), R.string.permission_denied, Toast.LENGTH_SHORT).show();
+        }
+    });
 }
--- app/src/main/java/nrw/florian/cookbook/MainActivity.java
+++ app/src/main/java/nrw/florian/cookbook/MainActivity.java
@@ -1,22 +1,19 @@
 package nrw.florian.cookbook;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import nrw.florian.cookbook.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import nrw.florian.cookbook.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
