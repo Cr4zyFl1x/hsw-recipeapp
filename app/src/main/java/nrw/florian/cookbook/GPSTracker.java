03ac77ae39f5958582a0d1cf6f1e4427989eacb6
RikiiB <143796850+RikiiB@users.noreply.github.com>
2024-05-28 23:27:05 +0200

    MAE-24 added Location and GPSTracker class

---
+++ app/src/main/java/nrw/florian/cookbook/GPSTracker.java
@@ -0,0 +1,55 @@
+package nrw.florian.cookbook;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.view.View;
+
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Optional;
+
+public class GPSTracker extends Activity {
+    private static final long MIN_DISTANCE_FOR_UPDATES = 1; // 10 meter
+    private static final long MIN_TIME_BETWEEN_UPDATES = 1; // 1 minute
+
+    nrw.florian.cookbook.Location.LocationBuilder locationBuilder;
+    private final FusedLocationProviderClient fusedLocationClient;
+
+    private Context context;
+
+    public GPSTracker(Context context) {
+        this.context = context;
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+    }
+
+    public Optional<nrw.florian.cookbook.Location> getGPSLocation(View view) {
+        this.locationBuilder = new nrw.florian.cookbook.Location.LocationBuilder();
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Snackbar.make(context, view, context.getString(R.string.no_permission_cannot_access_location),
+                            Snackbar.LENGTH_LONG)
+                    .setAction(R.string.permission_edit, click -> {
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", context.getPackageName(), null);
+                        intent.setData(uri);
+                        startActivity(intent);
+                    })
+                    .show();
+        } else {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
+                if (location != null) {
+                    locationBuilder.latitude(location.getLatitude());
+                    locationBuilder.longitude(location.getLongitude());
+                }
+            });
+        }
+        return Optional.of(locationBuilder.build());
+    }
+}
